008901d6b20d142b43ccf08e6a4e5faa8df9c3eb
Narcissus7 <18813104077@163.com>
2020-01-08 10:49:56 +0800

    494. 目标和

--- 2020.01.07-494.py
+++ 2020.01.07-494.py
@@ -62,25 +62,40 @@ class Solution:
     def findTargetSumWays3(self, nums: List[int], S: int) -> int:
         """
         转换成01背包问题
-        找到nums一个正子集和一个负子集，使得总和等于target，统计这种可能性的总数
+        找到nums一个正子集P和一个负子集N，使得总和等于target，统计这种可能性的总数
         https://leetcode-cn.com/problems/target-sum/solution/python-dfs-xiang-jie-by-jimmy00745/
+        假如给定nums = [num1,num2,num3]，我们现在可以理解dp[j] = dp[j-num1] + dp[j-num2] + dp[j-num3]。
         """
         total = sum(nums)
         if (total + S) % 2 != 0 or total < S:
             return 0
-        target = (total + S) // 2
-        dp = [1] + [0 for _ in range(target)]  # dp的第x项，代表组合成数字x有多少方法
+        P = (total + S) // 2
+        # dp的第x项，代表组合成数字x有多少方法
+        dp = [1] + [0 for _ in range(P)]
+        # 对每一个在nums数组中的数num而言，dp在从num到P的这些区间里，都可以加上一个num，来到达想要达成的P
         for num in nums:
-            for j in range(target, num - 1, -1):
+            for j in range(P, num - 1, -1):
                 dp[j] += dp[j - num]
-        return dp[target]
+        print(dp)
+        return dp[P]
+
+    def findTargetSumWays4(self, nums: List[int], S: int) -> int:
+        """
+        动态规划
+        dp[(i, j)]元祖第一位是目前的位数，第二位是目前的和
+        """
+        length, dp = len(nums), {(0, 0): 1}
+        for i in range(1, length + 1):
+            for j in range(-sum(nums), sum(nums) + 1):
+                dp[(i, j)] = dp.get((i - 1, j - nums[i - 1]), 0) + dp.get((i - 1, j + nums[i - 1]), 0)
+        return dp.get((length, S), 0)
 
 
 s = Solution()
 # print(s.findTargetSumWays([1, 1, 1, 1, 1], 3))
 # print(s.findTargetSumWays2([1, 1, 1, 1, 1], 3))
-# print(s.findTargetSumWays3([1, 1, 1, 1, 1], 3))
-print(s.findTargetSumWays([27, 22, 39, 22, 40, 32, 44, 45, 46, 8, 8, 21, 27, 8, 11, 29, 16, 15, 41, 0], 10))
-print(s.findTargetSumWays2([27, 22, 39, 22, 40, 32, 44, 45, 46, 8, 8, 21, 27, 8, 11, 29, 16, 15, 41, 0], 10))
-print(s.findTargetSumWays3([27, 22, 39, 22, 40, 32, 44, 45, 46, 8, 8, 21, 27, 8, 11, 29, 16, 15, 41, 0], 10))
+print(s.findTargetSumWays3([1, 2, 3, 4, 5], 3))
+# print(s.findTargetSumWays([27, 22, 39, 22, 40, 32, 44, 45, 46, 8, 8, 21, 27, 8, 11, 29, 16, 15, 41, 0], 10))
+# print(s.findTargetSumWays2([27, 22, 39, 22, 40, 32, 44, 45, 46, 8, 8, 21, 27, 8, 11, 29, 16, 15, 41, 0], 10))
+# print(s.findTargetSumWays3([27, 22, 39, 22, 40, 32, 44, 45, 46, 8, 8, 21, 27, 8, 11, 29, 16, 15, 41, 0], 10))
 
