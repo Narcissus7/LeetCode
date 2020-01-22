ff7fe189ceaba761cbc8732781a865d597a6bf9f
Narcissus7 <18813104077@163.com>
2020-01-22 11:21:38 +0800

    tmp

---
+++ 2020.01.21-714.py
@@ -0,0 +1,13 @@
+"""
+714. 买卖股票的最佳时机含手续费
+"""
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        pass
+
+
+s = Solution()
+print(s.maxProfit([1, 3, 2, 8, 4, 9], 2))
